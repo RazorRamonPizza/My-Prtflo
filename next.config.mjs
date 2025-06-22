d26469f1e28a322eca5010e834f2e1a414efb158
RazorRamonPizza <rv5@makarov@gmail.com>
2025-06-22 15:33:14 +0800

    Fix: Rename next.config.ts to next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Здесь можно будет добавлять другие настройки в будущем
+};
+
+export default nextConfig;
