e0b6b9d204b78c2b1ccda07679bddee31751683e
RazorRamonPizza <136936575+RazorRamonPizza@users.noreply.github.com>
2025-06-24 11:48:20 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,6 @@ function Gallery({ categories, works, onWorkClick }) {
                 title={work.title}
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerPolicy="strict-origin-when-cross-origin"
                 allowFullScreen
                 className="w-full h-full object-cover"
               ></iframe>
